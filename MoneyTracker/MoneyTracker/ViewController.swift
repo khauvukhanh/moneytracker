55e738cc042f1000577b405ef6e29f62725ec312
khanh <vukhanh.003@gmail.com>
2025-04-12 22:49:40 +0700

    init source

---
+++ MoneyTracker/MoneyTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyTracker
+//
+//  Created by Vu Khanh on 12/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
